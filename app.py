836d7b4b32835931befd1be4d2f37d9989f2055c
Ankit Sharma <ankitsharma1831ao@gmail.com>
2025-05-11 09:59:50 +0530

    env

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+load_dotenv()
+
+## Langsmith Tracking
+
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+## Prompt Template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With Groq Model")
+input_text=st.text_input("What question you have in mind?")
+
+
+## Ollama Llama2 model
+#llm=Ollama(model="gemma:2b")
+output_parser=StrOutputParser()
+chain=prompt|model|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
+
+
